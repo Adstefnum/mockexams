2a5d81509eef23a1c985bd6d799e8588cefa677b
C-NERD <alayaa694@gmail.com>
2021-03-17 14:01:24 +0100

    added some little changes

--- mockexams/static/js_src/navbar.js
+++ mockexams/static/js_src/navbar.js
@@ -3,9 +3,12 @@ import { h, text } from "https://unpkg.com/hyperapp"
 const changetheme = () =>
     document.getElementById('themechanger').classList.toggle('sun');
 
+const gohome = () =>
+    window.location.replace('/')
+
 export const nav = (name) =>
     h('nav', {id : 'navbar'}, [
-        h('span', {id : 'logopanel'}, [
+        h('span', {id : 'logopanel', onclick : gohome}, [
             h('div', {id : 'logo'}),
             h('label', {id : 'logoname'}, text(name))
         ]),
