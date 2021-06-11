6540130651c42981287c6391928f22fb055a5ceb
Adstefnum <bauerstevenson@gmail.com>
2021-06-11 15:12:19 -0400

    User auth apis done, add our specific fields to database and require them from register api and return them on login api sucess. Testing of all apis with external client should also commence

--- src/user_auth_api/urls.py
+++ src/user_auth_api/urls.py
@@ -1,5 +1,10 @@
-from django.contrib import admin
-from django.urls import path,include
+from user_auth_api.views import RegisterAPI,LoginAPI
+from django.urls import path
+from knox import views as knox_views
 
 urlpatterns = [
+    path('users/register/v1/', RegisterAPI.as_view(), name='register'),
+     path('users/login/v1/', LoginAPI.as_view(), name='login'),
+    path('users/logout/v1/', knox_views.LogoutView.as_view(), name='logout'),
+    path('users/logoutall/v1/', knox_views.LogoutAllView.as_view(), name='logoutall'),
 ]
