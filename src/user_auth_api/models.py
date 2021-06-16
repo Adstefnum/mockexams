9946022b5a265797bf3752752452a8394b2d4aac
Adstefnum <bauerstevenson@gmail.com>
2021-06-15 21:14:17 -0400

    pushing venv

--- src/user_auth_api/models.py
+++ src/user_auth_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class userProfile(models.Model):
+	user = models.OneToOneField(User,on_delete=models.CASCADE)
+	uuid = models.CharField(max_length=16)
+	phone = models.CharField(max_length=16)
+	image = models.ImageField(upload_to='media/profile_pics/')
+
+class userRecord(models.Model):
+	pass
+
