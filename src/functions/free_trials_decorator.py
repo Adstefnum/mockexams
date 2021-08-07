371bc6e390df5002296b6dc73adc254c82c4a37f
Adstefnum <bauerstevenson@gmail.com>
2021-08-07 07:24:36 -0400

    Fixed the payment redirect bug

--- src/functions/free_trials_decorator.py
+++ src/functions/free_trials_decorator.py
@@ -1,7 +1,7 @@
-from user_auth_api.models import UserRecord
+from user_auth_api.models import userRecord
 from django.shortcuts import redirect
 
 def free_trials_decorator(request):
 	current_user_id = request.user.id
-	if UserRecord.objects.filter(user__id=current_user_id).count() == 5 and user.has_paid = False:
+	if userRecord.objects.filter(user__id=current_user_id).count() == 5 and user.has_paid == False:
 		redirect('mockexamsapp:payment')
--- src/mockexamsapp/views.py
+++ src/mockexamsapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from user_auth_api.models import UserRecord
+from user_auth_api.models import userRecord
 
 
 class IndexView(ListView):
@@ -16,7 +16,7 @@ class IndexView(ListView):
 
 	def exam(self, request, examid, userid):
 		current_user_id = request.user.id
-		if UserRecord.objects.filter(user__id=current_user_id).count() == 5 and user.has_paid = False:
+		if userRecord.objects.filter(user__id=current_user_id).count() == 5 and user.has_paid == False:
 			return self.payment(request)
 		else:
 			static = {'js' : 'exam', 'css' : 'exam'}
