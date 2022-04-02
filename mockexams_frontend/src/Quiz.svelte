6b51677bb5eddfb8615ba6e4628f18e9e54065b6
Adstefnum <bauerstevenson@gmail.com>
2022-04-02 21:56:57 +0300

    setting up frontend in svelte

---
+++ mockexams_frontend/src/Quiz.svelte
@@ -0,0 +1,10 @@
+<script>
+import Options from './Options.svelte';
+import Question from './Question.svelte';
+</script>
+
+<h1>
+	quiz is firing
+</h1>
+<Options />
+<Question />
---
+++ mockexams_frontend/src/Timer.svelte
@@ -0,0 +1,7 @@
+<script>
+ function timer(){
+	 setTimeout
+ }
+
+
+</script>
